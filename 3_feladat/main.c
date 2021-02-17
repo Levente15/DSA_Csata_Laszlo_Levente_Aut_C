a5f3f6318e73321b85fa28473d6d0dccaea21a06
Levente15 <79134762+Levente15@users.noreply.github.com>
2021-02-17 10:34:32 +0200

    3_feladat

--- 3_feladat/main.c
+++ 3_feladat/main.c
@@ -1,6 +1,14 @@
 #include <stdio.h>
 
+int ossz(int sz1,int sz2);
 int main() {
-    
+    int sz1=0,sz2=0;
+    scanf("%i",&sz1);
+    scanf("%i",&sz2);
+    printf("Az ossz: %i", ossz(sz1,sz2));
     return 0;
+}
+int ossz(int sz1,int sz2)
+{
+    return sz1+sz2;
 }
