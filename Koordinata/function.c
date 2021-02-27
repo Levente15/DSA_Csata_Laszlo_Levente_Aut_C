bb1621fd9e62c4e5cf6b23ff984a789c0b48684f
Levente15 <79134762+Levente15@users.noreply.github.com>
2021-02-27 17:15:41 +0200

    Delete Koordinata directory

--- Koordinata/function.c
+++
@@ -1,5 +0,0 @@
-//
-// Created by Legion on 2/27/2021.
-//
-
-#include "function.h"
--- Koordinata/function.h
+++
@@ -1,8 +0,0 @@
-//
-// Created by Legion on 2/27/2021.
-//
-
-#ifndef KOORDINATA_FUNCTION_H
-#define KOORDINATA_FUNCTION_H
-
-#endif //KOORDINATA_FUNCTION_H
