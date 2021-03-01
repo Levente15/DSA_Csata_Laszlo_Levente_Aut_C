d226bece51a5b78ef063b03af54988bb21a42296
Levente15 <79134762+Levente15@users.noreply.github.com>
2021-03-01 14:14:55 +0200

    Linearis

--- Labor_2/feladat_1/main.c
+++ Labor_2/feladat_1/main.c
@@ -5,18 +5,19 @@
 #include "function.h"
 
 int main() {
+    clock_t start = clock();
     srand(time(NULL));
     int n;
-    scanf("%i",&n);
+    scanf("%i", &n);
 
-    int* a=(int*)malloc(n*sizeof(int));
-    feltoltes(a,n);
-    int keresettelem;
-    printf("Add meg a keresett elemet:");
-    scanf("%i",&keresettelem);
-    kiiras(a,n);
-    printf("\nA helye: %i",lineariskereses(a,n,keresettelem));
+    int *a = (int *) malloc(n * sizeof(int));
+    feltoltes(a, n);
+    int keresettelem = rand() % 100;
 
+    printf("\nA helye: %i", lineariskereses(a, n, keresettelem));
+    clock_t stop = clock();
+    double elapsed = (double) (stop - start) / CLOCKS_PER_SEC;
+    printf("\nLefutasi ido: %.5f\n", elapsed);
 
     return 0;
 }
