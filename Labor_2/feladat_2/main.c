3d853bb9f48d3032f1f8ded63c35073d7250deb0
Levente15 <79134762+Levente15@users.noreply.github.com>
2021-03-02 19:05:52 +0200

    Binariss

--- Labor_2/feladat_2/main.c
+++ Labor_2/feladat_2/main.c
@@ -13,14 +13,12 @@ int main() {
     int* a=(int*)malloc(n*sizeof(int));
     int elso=0,utolso=n-1;
     feltoltes(a,n);
-    kiiras(a,n);
+    //kiiras(a,n);
     printf("\nA keresett elem: %i\n",keresettelem);
     qsort(a,n,sizeof(int),seged);
-    kiiras(a,n);
+    //kiiras(a,n);
     binariskereses(a,n,elso,utolso,keresettelem);
     free(a);
-    clock_t stop = clock();
-    double elapsed = (double) (stop - start) / CLOCKS_PER_SEC;
-    printf("\nTime : %.5f\n", elapsed);
+
     return 0;
 }
