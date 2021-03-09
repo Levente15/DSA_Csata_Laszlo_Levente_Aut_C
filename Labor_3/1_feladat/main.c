2e85b8e4c93e88a29377de363e9ea51908bcf7ee
Levente15 <79134762+Levente15@users.noreply.github.com>
2021-03-09 14:26:09 +0200

    1

--- Labor_3/1_feladat/main.c
+++ Labor_3/1_feladat/main.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
 #include "function.h"
 int main() {
-    FILE *fp;
-    fp = fopen("be.txt", "r");
+    /*HIBAS A FELADAT, MERT NEM TUDTAM MI A FELADAT PONTOSAN
+    //FILE *fp;
+    //fp = fopen("be.txt", "r");
 
     int n;
     printf("A dolgozok szama: ");
