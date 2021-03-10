a9ba692e9f8ca72c5446d584f9c1dc5eef8fb89c
Levente15 <79134762+Levente15@users.noreply.github.com>
2021-03-10 11:58:28 +0200

    1

---
+++ Labor_3/1feladat/functions.h
@@ -0,0 +1,10 @@
+//
+// Created by Legion on 3/10/2021.
+//
+
+#ifndef INC_1FELADAT_FUNCTIONS_H
+#define INC_1FELADAT_FUNCTIONS_H
+void feltoltes(int**a, int n);
+void feltoltes1(int**b, int m);
+void szorzas(int**a,int**b, int n,int m,int**s);
+#endif //INC_1FELADAT_FUNCTIONS_H
