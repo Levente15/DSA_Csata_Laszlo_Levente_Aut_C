ec14e15c8c4b959f74c0af022bc06c271186a842
Levente15 <79134762+Levente15@users.noreply.github.com>
2021-04-06 17:04:39 +0300

    Delete Labor_7 directory

--- Labor_7/Beolvasas_proba/main.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-#include "functions.h"
-int main() {
-    Szemely *szemely=readfromfile("be.txt");
-    for(int i=0;i<5;i++){
-        printf("%s %s %i\n",szemely[i].vnev,szemely[i].knev,szemely[i].kor);
-    }
-    return 0;
-}
